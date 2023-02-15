3811468a0d1a50ae88386f6e63329b62e28eec57
Gouhjack <122753982+Gouhjack@users.noreply.github.com>
2023-02-15 16:34:26 +0100

    début

---
+++ Assets/Scripts/PlayerLook.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+    [SerializeField] private Vector2 _mouseSensitivity;
+    [SerializeField] private Vector2 _padSensitivity;
+    [SerializeField] private Vector2 _mouseYLimit;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
